22dba698b0bb3ab979c5f0ce5fa1a3c7eb2a1586
Aman Agrawal <amanagarwal994@gmail.com>
2026-05-15 00:03:57 +0530

    Episode 4 FoodApp- Restaurant Card Creation ( Raw )

--- App.js
+++ App.js
@@ -1,29 +1,96 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX HTML like syntax or XMl like syntax.
-// JSX code is transpiled before it reaches to JS engine  - > Parcel -> babel does that job
 
-// JSX => React.createElement - JS object => HtmlElem'ent(render)
-const Title = () => (
-  <h1 className="head" tabIndex="1">
-    Hello World From REACT
-  </h1>
-);
+/*
+1. HeaderComponent
+  - Logo
+  - Nav Items
 
-// React Components ?
-// Two ways:
-// Class based Component -  old
-// function based Component - new
-//Component Composition.
-const HeadingComponent = () => {
+2. Body
+  - Search 
+  - Restaurant Container
+  - Restaurant Card
+    - Image
+    - Name
+    - Cuisine
+    - Rating
+3. Footer
+  - Links
+  - Copyright
+  - Contact
+  - Address
+
+  Always build from top level component.
+*/
+
+const HeaderComponent = () => {
   return (
-    <div className="container">
-      <Title />
-      <h1> Welcome from Functional Component</h1>
+    <div className = "header">
+      <div className = "logo-container">
+        <div className="logo">
+          <img src="http://logodesign.net/logo-new/burger-with-fries-on-side-7182ld.png?nwm=1&nws=1&industry=burger-fries&txt_keyword=All" alt="logo" className="logo"/>
+        </div>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
-  );
+  )
 };
 
+
+const RestaurantCard = () =>{
+  return (
+    <div className = "res-card">
+        <img className="res-logo" alt="Restaurant Logo" src="https://www.tastingtable.com/img/gallery/20-delicious-indian-dishes-you-have-to-try-at-least-once/l-intro-1733153567.jpg"></img>
+        <h3>Atithi</h3>
+        <h4>North Indian, Chinese</h4>
+        <h4>4.5 Stars</h4>
+        <h4>25 minutes</h4>
+    </div>
+  )
+};
+const Body = () => {
+  return(
+      <div className = "body">
+          <div className="search">
+            Search 
+          </div>
+          <div className="res-container">
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+              <RestaurantCard />
+
+          </div>
+      </div>
+  )
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app-layout">
+      <HeaderComponent />
+      <Body/>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent/>);
+root.render(<AppLayout/>);
